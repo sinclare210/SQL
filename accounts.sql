839825f5f6430d2f549b1d310d55e60bfb8ef0fe
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-08-14 16:52:15 +0100

    (feature):Acid transaction n account

---
+++ accounts.sql
@@ -0,0 +1,117 @@
+create table accounts (
+	id BIGSERIAL PRIMARY KEY NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(50),
+	balance INT NOT NULL
+);
+insert into accounts (id, first_name, last_name, email, balance) values (1, 'Kristina', 'Stroobant', 'kstroobant0@sogou.com', 8331);
+insert into accounts (id, first_name, last_name, email, balance) values (2, 'Gwyn', 'Lockhart', 'glockhart1@ftc.gov', 9320);
+insert into accounts (id, first_name, last_name, email, balance) values (3, 'Jacquie', 'Videan', 'jvidean2@usatoday.com', 7568);
+insert into accounts (id, first_name, last_name, email, balance) values (4, 'Raeann', 'Riddle', 'rriddle3@vinaora.com', 8006);
+insert into accounts (id, first_name, last_name, email, balance) values (5, 'Nat', 'Chetter', null, 1014);
+insert into accounts (id, first_name, last_name, email, balance) values (6, 'Mauricio', 'Cadding', null, 1875);
+insert into accounts (id, first_name, last_name, email, balance) values (7, 'Julius', 'Paviour', 'jpaviour6@livejournal.com', 1215);
+insert into accounts (id, first_name, last_name, email, balance) values (8, 'Dieter', 'Flinn', 'dflinn7@toplist.cz', 1145);
+insert into accounts (id, first_name, last_name, email, balance) values (9, 'Andreana', 'Curlis', 'acurlis8@vkontakte.ru', 6905);
+insert into accounts (id, first_name, last_name, email, balance) values (10, 'Markus', 'Wardhough', 'mwardhough9@blogtalkradio.com', 3178);
+insert into accounts (id, first_name, last_name, email, balance) values (11, 'Mellisa', 'Westcar', 'mwestcara@mlb.com', 8784);
+insert into accounts (id, first_name, last_name, email, balance) values (12, 'Auria', 'Rudeforth', 'arudeforthb@nbcnews.com', 8835);
+insert into accounts (id, first_name, last_name, email, balance) values (13, 'Wilhelmina', 'Krugmann', 'wkrugmannc@booking.com', 4695);
+insert into accounts (id, first_name, last_name, email, balance) values (14, 'Gilberte', 'Etienne', 'getienned@jimdo.com', 5597);
+insert into accounts (id, first_name, last_name, email, balance) values (15, 'Nicholle', 'Stellino', 'nstellinoe@hao123.com', 8616);
+insert into accounts (id, first_name, last_name, email, balance) values (16, 'Artair', 'De la Yglesias', 'adelayglesiasf@php.net', 3021);
+insert into accounts (id, first_name, last_name, email, balance) values (17, 'Allison', 'Sphinxe', 'asphinxeg@fastcompany.com', 3256);
+insert into accounts (id, first_name, last_name, email, balance) values (18, 'Nicolais', 'Dimmock', null, 4573);
+insert into accounts (id, first_name, last_name, email, balance) values (19, 'Stacy', 'Mansion', null, 3743);
+insert into accounts (id, first_name, last_name, email, balance) values (20, 'Inesita', 'Pelchat', 'ipelchatj@tuttocitta.it', 5397);
+insert into accounts (id, first_name, last_name, email, balance) values (21, 'Tabor', 'McBain', 'tmcbaink@bloomberg.com', 4126);
+insert into accounts (id, first_name, last_name, email, balance) values (22, 'Hetti', 'Velareal', null, 9081);
+insert into accounts (id, first_name, last_name, email, balance) values (23, 'Oswald', 'Bawle', 'obawlem@google.fr', 8523);
+insert into accounts (id, first_name, last_name, email, balance) values (24, 'Burnard', 'Danilyuk', 'bdanilyukn@timesonline.co.uk', 5170);
+insert into accounts (id, first_name, last_name, email, balance) values (25, 'Timothee', 'Shellum', 'tshellumo@list-manage.com', 8121);
+insert into accounts (id, first_name, last_name, email, balance) values (26, 'Aaren', 'Briand', 'abriandp@flavors.me', 6512);
+insert into accounts (id, first_name, last_name, email, balance) values (27, 'Merline', 'Sweeney', 'msweeneyq@icio.us', 1099);
+insert into accounts (id, first_name, last_name, email, balance) values (28, 'Courtnay', 'Varfalameev', 'cvarfalameevr@dedecms.com', 7270);
+insert into accounts (id, first_name, last_name, email, balance) values (29, 'Pattie', 'Amsden', null, 2004);
+insert into accounts (id, first_name, last_name, email, balance) values (30, 'Damiano', 'Tomczak', null, 5673);
+insert into accounts (id, first_name, last_name, email, balance) values (31, 'Maryanna', 'MacCosty', 'mmaccostyu@vinaora.com', 5887);
+insert into accounts (id, first_name, last_name, email, balance) values (32, 'Sammy', 'Rubinfeld', null, 4302);
+insert into accounts (id, first_name, last_name, email, balance) values (33, 'Lionel', 'Halbard', 'lhalbardw@oakley.com', 8659);
+insert into accounts (id, first_name, last_name, email, balance) values (34, 'Rebecca', 'Primmer', 'rprimmerx@fema.gov', 8583);
+insert into accounts (id, first_name, last_name, email, balance) values (35, 'Hyatt', 'Billborough', 'hbillboroughy@wisc.edu', 3572);
+insert into accounts (id, first_name, last_name, email, balance) values (36, 'Wilmette', 'Crookall', 'wcrookallz@comsenz.com', 9645);
+insert into accounts (id, first_name, last_name, email, balance) values (37, 'Shell', 'Spradbrow', null, 3775);
+insert into accounts (id, first_name, last_name, email, balance) values (38, 'Ara', 'Dimitrov', 'adimitrov11@t-online.de', 9536);
+insert into accounts (id, first_name, last_name, email, balance) values (39, 'Bianka', 'Dominey', 'bdominey12@tripod.com', 6967);
+insert into accounts (id, first_name, last_name, email, balance) values (40, 'Anna', 'Pinke', 'apinke13@wikispaces.com', 6079);
+insert into accounts (id, first_name, last_name, email, balance) values (41, 'Clementia', 'Landers', 'clanders14@slashdot.org', 5523);
+insert into accounts (id, first_name, last_name, email, balance) values (42, 'Jayme', 'Vernon', null, 8059);
+insert into accounts (id, first_name, last_name, email, balance) values (43, 'Berky', 'Goldsberry', 'bgoldsberry16@cbslocal.com', 2485);
+insert into accounts (id, first_name, last_name, email, balance) values (44, 'Ingunna', 'Briscam', 'ibriscam17@phpbb.com', 3515);
+insert into accounts (id, first_name, last_name, email, balance) values (45, 'Trix', 'Tomczynski', 'ttomczynski18@over-blog.com', 6359);
+insert into accounts (id, first_name, last_name, email, balance) values (46, 'Darb', 'Rawle', 'drawle19@google.ca', 9196);
+insert into accounts (id, first_name, last_name, email, balance) values (47, 'Khalil', 'Island', null, 2420);
+insert into accounts (id, first_name, last_name, email, balance) values (48, 'Jolene', 'Edmonston', 'jedmonston1b@omniture.com', 2263);
+insert into accounts (id, first_name, last_name, email, balance) values (49, 'Catie', 'Oleszkiewicz', 'coleszkiewicz1c@paginegialle.it', 9219);
+insert into accounts (id, first_name, last_name, email, balance) values (50, 'Sander', 'Griswood', null, 1609);
+insert into accounts (id, first_name, last_name, email, balance) values (51, 'Alvira', 'Carnew', 'acarnew1e@wikipedia.org', 9335);
+insert into accounts (id, first_name, last_name, email, balance) values (52, 'Adrea', 'Dredge', 'adredge1f@zimbio.com', 3370);
+insert into accounts (id, first_name, last_name, email, balance) values (53, 'Cody', 'Tinkham', 'ctinkham1g@marriott.com', 3713);
+insert into accounts (id, first_name, last_name, email, balance) values (54, 'Nanice', 'Saywell', null, 5381);
+insert into accounts (id, first_name, last_name, email, balance) values (55, 'Hieronymus', 'Rockey', 'hrockey1i@squarespace.com', 2701);
+insert into accounts (id, first_name, last_name, email, balance) values (56, 'Nikolos', 'Cristou', 'ncristou1j@businessweek.com', 9015);
+insert into accounts (id, first_name, last_name, email, balance) values (57, 'Stillmann', 'Gosden', 'sgosden1k@tamu.edu', 4242);
+insert into accounts (id, first_name, last_name, email, balance) values (58, 'Barret', 'Ganny', 'bganny1l@goo.ne.jp', 4597);
+insert into accounts (id, first_name, last_name, email, balance) values (59, 'Babs', 'Godman', 'bgodman1m@senate.gov', 8600);
+insert into accounts (id, first_name, last_name, email, balance) values (60, 'Mariann', 'Glanville', 'mglanville1n@wsj.com', 2406);
+insert into accounts (id, first_name, last_name, email, balance) values (61, 'Eugenie', 'Mulvany', 'emulvany1o@e-recht24.de', 6778);
+insert into accounts (id, first_name, last_name, email, balance) values (62, 'Courtnay', 'Stangoe', 'cstangoe1p@discuz.net', 7191);
+insert into accounts (id, first_name, last_name, email, balance) values (63, 'Putnem', 'Dollard', 'pdollard1q@oaic.gov.au', 4635);
+insert into accounts (id, first_name, last_name, email, balance) values (64, 'Denys', 'O''Cosgra', 'docosgra1r@cnet.com', 2191);
+insert into accounts (id, first_name, last_name, email, balance) values (65, 'Gustavus', 'McPharlain', 'gmcpharlain1s@prnewswire.com', 1201);
+insert into accounts (id, first_name, last_name, email, balance) values (66, 'Audie', 'Blomefield', 'ablomefield1t@google.fr', 2014);
+insert into accounts (id, first_name, last_name, email, balance) values (67, 'Kara-lynn', 'Corneljes', 'kcorneljes1u@narod.ru', 9999);
+insert into accounts (id, first_name, last_name, email, balance) values (68, 'Adiana', 'Yukhtin', 'ayukhtin1v@nps.gov', 2387);
+insert into accounts (id, first_name, last_name, email, balance) values (69, 'Adara', 'Sessuns', 'asessuns1w@odnoklassniki.ru', 7860);
+insert into accounts (id, first_name, last_name, email, balance) values (70, 'Saudra', 'De Leek', 'sdeleek1x@t-online.de', 3365);
+insert into accounts (id, first_name, last_name, email, balance) values (71, 'Sunny', 'MacCallum', 'smaccallum1y@pen.io', 1512);
+insert into accounts (id, first_name, last_name, email, balance) values (72, 'Kameko', 'Shelsher', 'kshelsher1z@pagesperso-orange.fr', 5371);
+insert into accounts (id, first_name, last_name, email, balance) values (73, 'Wells', 'Aimeric', null, 8884);
+insert into accounts (id, first_name, last_name, email, balance) values (74, 'Heddie', 'Cooksey', 'hcooksey21@devhub.com', 4185);
+insert into accounts (id, first_name, last_name, email, balance) values (75, 'Lisbeth', 'Guyers', null, 4168);
+insert into accounts (id, first_name, last_name, email, balance) values (76, 'Daryn', 'Mallaby', 'dmallaby23@pen.io', 8642);
+insert into accounts (id, first_name, last_name, email, balance) values (77, 'Oralla', 'Edison', null, 1656);
+insert into accounts (id, first_name, last_name, email, balance) values (78, 'Melody', 'Matzl', 'mmatzl25@zdnet.com', 5067);
+insert into accounts (id, first_name, last_name, email, balance) values (79, 'Sandie', 'Glasgow', 'sglasgow26@springer.com', 4507);
+insert into accounts (id, first_name, last_name, email, balance) values (80, 'Cele', 'Wheeler', 'cwheeler27@xrea.com', 7860);
+insert into accounts (id, first_name, last_name, email, balance) values (81, 'Conrade', 'Andrat', null, 9136);
+insert into accounts (id, first_name, last_name, email, balance) values (82, 'Ludvig', 'Jovasevic', null, 2562);
+insert into accounts (id, first_name, last_name, email, balance) values (83, 'Maribelle', 'Yurinov', 'myurinov2a@msn.com', 3001);
+insert into accounts (id, first_name, last_name, email, balance) values (84, 'Iorgo', 'Pidcock', 'ipidcock2b@soup.io', 9579);
+insert into accounts (id, first_name, last_name, email, balance) values (85, 'Frederico', 'Trouel', 'ftrouel2c@github.com', 3247);
+insert into accounts (id, first_name, last_name, email, balance) values (86, 'Dino', 'O''Lagen', 'dolagen2d@cbslocal.com', 2727);
+insert into accounts (id, first_name, last_name, email, balance) values (87, 'Barty', 'Tremathick', 'btremathick2e@tiny.cc', 4299);
+insert into accounts (id, first_name, last_name, email, balance) values (88, 'Shaine', 'Cornes', null, 4318);
+insert into accounts (id, first_name, last_name, email, balance) values (89, 'Marni', 'Mc Dermid', 'mmcdermid2g@abc.net.au', 9551);
+insert into accounts (id, first_name, last_name, email, balance) values (90, 'Kalle', 'Kieran', 'kkieran2h@berkeley.edu', 3273);
+insert into accounts (id, first_name, last_name, email, balance) values (91, 'Nicolai', 'Matchell', null, 9179);
+insert into accounts (id, first_name, last_name, email, balance) values (92, 'Kiley', 'Knox', 'kknox2j@washington.edu', 1567);
+insert into accounts (id, first_name, last_name, email, balance) values (93, 'Ali', 'Dorracott', 'adorracott2k@unc.edu', 5171);
+insert into accounts (id, first_name, last_name, email, balance) values (94, 'Rooney', 'Gyrgorcewicx', null, 2478);
+insert into accounts (id, first_name, last_name, email, balance) values (95, 'Sanson', 'Wauchope', 'swauchope2m@g.co', 4031);
+insert into accounts (id, first_name, last_name, email, balance) values (96, 'Ashleigh', 'Scade', 'ascade2n@rediff.com', 9289);
+insert into accounts (id, first_name, last_name, email, balance) values (97, 'Chandra', 'Canham', null, 2861);
+insert into accounts (id, first_name, last_name, email, balance) values (98, 'Shelley', 'Veasey', null, 5041);
+insert into accounts (id, first_name, last_name, email, balance) values (99, 'Templeton', 'Deeley', 'tdeeley2q@is.gd', 4790);
+insert into accounts (id, first_name, last_name, email, balance) values (100, 'Katusha', 'Isenor', 'kisenor2r@ask.com', 7445);
+
+BEGIN;
+
+UPDATE accounts SET balance = balance - 7000 WHERE id = 100;
+
+UPDATE accounts SET balance = balance + 7000 WHERE id = 99;
+
+COMMIT;
+
+ROLLBACK;
